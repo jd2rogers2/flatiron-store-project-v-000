ecedb1f9b60444022ae99355cc584f39d3bdf612
Jesse Novotny <jessenovotny@gmail.com>
2016-09-12 15:26:58 -0700

    everything but cart_feature_spec

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # def current_cart
+  #   # binding.pry
+  #   current_user.current_cart ||= Cart.create
+  #   current_user.save
+  #   current_user.current_cart
+  #   # binding.pry
+  # end
+
 end
